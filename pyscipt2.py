3c8773fc29dd962dfb90c11b8b1d38c5e1c4cf28
Prateek Bisht <105870180+PratiikBisht@users.noreply.github.com>
2024-03-08 21:11:30 +0530

    Update pyscipt2.py

--- pyscipt2.py
+++ pyscipt2.py
@@ -8,3 +8,21 @@ rc = RobotControl()
 a = rc.get_laser(num)
 
 print ("The laser value received is: ", a)
+
+
+"""" the same above program  with little changes like it gets stopped before 1 meter and its logic is in while loop ""
+
+from robot_control_class import RobotControl
+
+robotcontrol = RobotControl()
+
+a = robotcontrol.get_laser(360)
+
+while a > 1:
+    robotcontrol.move_straight()
+    a = robotcontrol.get_laser(360)
+    print ("Current distance to wall: %f" % a)
+
+robotcontrol.stop_robot()
+
+print ("Wall is at %f meters! Stop the robot!" % a)
