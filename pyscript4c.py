c73c217a8d4dc4ca6ef45513da2ab489c4004399
Prateek Bisht <105870180+PratiikBisht@users.noreply.github.com>
2024-03-09 12:53:06 +0530

    Create pyscript4c.py

---
+++ pyscript4c.py
@@ -0,0 +1,7 @@
+'''making the orbot move with parameters'''
+from robot_control_class import RobotControl
+
+robotcontrol = RobotControl(robot_name="summit")
+
+robotcontrol.move_straight_time("forward", 0.3, 5)
+robotcontrol.turn("clockwise", 0.3, 7)
