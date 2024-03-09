cb33849fe1c2c3cbe35a24f26ef44a97b5ac7fa1
Prateek Bisht <105870180+PratiikBisht@users.noreply.github.com>
2024-03-09 12:56:13 +0530

    Update pyscript4c.py

--- pyscript4c.py
+++ pyscript4c.py
@@ -5,3 +5,13 @@ robotcontrol = RobotControl(robot_name="summit")
 
 robotcontrol.move_straight_time("forward", 0.3, 5)
 robotcontrol.turn("clockwise", 0.3, 7)
+
+
+from robot_control_class import RobotControl
+
+robotcontrol = RobotControl(robot_name="summit")
+
+robotcontrol.turn("counter-clockwise", 0.3, 4)
+robotcontrol.move_straight_time("forward", 0.3, 6)
+robotcontrol.turn("counter-clockwise", 0.3, 4)
+robotcontrol.move_straight_time("forward", 0.3, 7)
